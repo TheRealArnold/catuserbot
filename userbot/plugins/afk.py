22f3dd6abf8f479b8cf4efdb91386acf467d1d52
subinps <64341611+subinps@users.noreply.github.com>
2021-06-14 19:34:45 +0530

    ___

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -113,23 +113,23 @@ async def on_afk(event):  # sourcery no-metrics
         if AFK_.afk_type == "media":
             if AFK_.reason:
                 message_to_reply = (
-                    f"`I am AFK .\n\nAFK Since {endtime}\nReason : {AFK_.reason}`"
+                    f"`My Master SUBIN is AFK(Away From Keybord) and can't talk to you now. He will be repliyng to your message when he is back online.\n\nAFK Since {endtime}\nReason : {AFK_.reason}`"
                 )
             else:
-                message_to_reply = f"`I am AFK .\n\nAFK Since {endtime}\nReason : Not Mentioned ( ಠ ʖ̯ ಠ)`"
+                message_to_reply = f"`My Master SUBIN is AFK(Away From Keybord) and can't talk to you now. He will be repliyng to your message when he is back online.\n\nAFK Since {endtime}\nReason : Not Mentioned ( ಠ ʖ̯ ಠ)`"
             if event.chat_id:
                 msg = await event.reply(message_to_reply, file=AFK_.media_afk.media)
         elif AFK_.afk_type == "text":
             if AFK_.msg_link and AFK_.reason:
                 message_to_reply = (
-                    f"**I am AFK .\n\nAFK Since {endtime}\nReason : **{AFK_.reason}"
+                    f"**My Master SUBIN is AFK(Away From Keybord) and can't talk to you now. He will be repliyng to your message when he is back online.\n\nAFK Since {endtime}\nReason : **{AFK_.reason}"
                 )
             elif AFK_.reason:
                 message_to_reply = (
-                    f"`I am AFK .\n\nAFK Since {endtime}\nReason : {AFK_.reason}`"
+                    f"`My Master SUBIN is AFK(Away From Keybord) and can't talk to you now. He will be repliyng to your message when he is back online.\n\nAFK Since {endtime}\nReason : {AFK_.reason}`"
                 )
             else:
-                message_to_reply = f"`I am AFK .\n\nAFK Since {endtime}\nReason : Not Mentioned ( ಠ ʖ̯ ಠ)`"
+                message_to_reply = f"`My Master SUBIN is AFK(Away From Keybord) and can't talk to you now. He will be repliyng to your message when he is back online.\n\nAFK Since {endtime}\nReason : Not Mentioned ( ಠ ʖ̯ ಠ)`"
             if event.chat_id:
                 msg = await event.reply(message_to_reply)
         if event.chat_id in AFK_.last_afk_message:
